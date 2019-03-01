c37c6f48dd66e4d64c3a48b1d8a664c86a8a1315
Akos Birmacher <akos.birmacher@bitrise.io>
2019-03-01 10:19:05 +0100

    add mac without shared scheme to test Bitrise scanner

---
+++ MacProvisioningProfileTest/MacProvisioningProfileTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacProvisioningProfileTest
+//
+//  Created by Akos Birmacher on 2018. 07. 05..
+//  Copyright © 2018. Bitfall. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacProvisioningProfileTest/MacProvisioningProfileTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacProvisioningProfileTest
+//
+//  Created by Akos Birmacher on 2018. 07. 05..
+//  Copyright © 2018. Bitfall. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
